dc8c33121201c09bbe381efd05da65fb6c52f6c0
Rene Schwietzke <r.schwietzke@xceptance.net>
2022-01-05 18:42:36 +0100

    ParseBoolean finalized

--- src/main/java/com/xceptance/common/lang/ParseBoolean.java
+++ src/main/java/com/xceptance/common/lang/ParseBoolean.java
@@ -6,11 +6,14 @@ public class ParseBoolean
 {
 	/**
 	 * Parses chars and evaluates if this is a boolean. Anything that is not true or TRUE
-	 * or anything like True will evaluate to false
+	 * or similar to True will evaluate to false. This is optimized for speed.
+	 * 
+	 * @param b buffer with characters, null is not permitted
+	 * @return true when chars match case-insensitive, false in any other case
 	 */
 	public static boolean parse(final XltCharBuffer b)
 	{
-		// length is incorrect, it is false
+		// length is incorrect, it must be false
 		if (b.length() != 4)
 		{
 			return false;
@@ -25,6 +28,7 @@ public class ParseBoolean
 		// fastpath and slowpath
 		final boolean b1 = (t == 't' & r == 'r' & u == 'u' & e == 'e');
 		
+		// slowpath will only be taken when needed, expected most true/false char buffers to be lowercase only
 		return b1 ? true : ((t == 't' || t == 'T') && (r == 'r' || r == 'R') && (u == 'u' || u == 'U') && (e == 'e' || e == 'E'));
 	}
 }
--- src/test/java/com/xceptance/common/lang/ParseBooleanTest.java
+++ src/test/java/com/xceptance/common/lang/ParseBooleanTest.java
@@ -1,26 +1,33 @@
 package com.xceptance.common.lang;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.xceptance.common.util.XltCharBuffer;
 
-import static org.junit.Assert.*;
-
 public class ParseBooleanTest 
 {
-	@Test
-	public void normal()
-	{
-		assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("true")));
-		assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("false")));
-		assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("trueish")));
-		assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("wahr")));
+    @Test
+    public void normal()
+    {
+        assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("true")));
+        assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("false")));
+        assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("trueish")));
+        assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("wahr")));
+        assertFalse(ParseBoolean.parse(XltCharBuffer.valueOf("")));
 
-		assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("TRUE")));
-		assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("truE")));
-		assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("True")));
-		assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("tRue")));
-		assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("trUe")));
+        assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("TRUE")));
+        assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("truE")));
+        assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("True")));
+        assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("tRue")));
+        assertTrue(ParseBoolean.parse(XltCharBuffer.valueOf("trUe")));
+    }
 
-	}
+    @Test(expected = NullPointerException.class)
+    public void nullNPE()
+    {
+        assertTrue(ParseBoolean.parse(null));
+    }
 }
